e8caf2e5958d23670fa3541daabe58274224c981
Brent Labasan <BrentLabasan@gmail.com>
2011-11-03 15:36:20 -0700

    URL now pops up when you click the extension's icon

--- contentscript.js
+++ contentscript.js
@@ -2,4 +2,12 @@
  * @author Brent Labasan
  */
 
-alert("content script")
+// alert("content script") // pops up every time a new page is loaded
+
+function displayTabURL() {
+	chrome.tabs.getSelected(null, function(tab) {
+                  alert(tab.url);
+        });
+}
+
+displayTabURL();
