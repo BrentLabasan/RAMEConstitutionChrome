9d78b1eaf4d75aa1e6ed50aa3e0194dac31f3cbd
Brent Labasan <BrentLabasan@gmail.com>
2011-11-03 22:14:45 -0700

    contentscript.js has been set up
    it will inject the button directly onto the news pages

--- contentscript.js
+++ contentscript.js
@@ -2,9 +2,9 @@
  * @author Brent Labasan
  */
 
-alert("content script") // pops up every time a new page is loaded
+alert("content script") ;// pops up every time a new page is loaded
 // document.getElementById('header-img-a').innerHTML = 'Fred Flinstone'; // works on reddit.com
-alert("end of content script");
+
 
 /* // doesn't work
 function displayTabURL() {
@@ -16,3 +16,34 @@ function displayTabURL() {
 displayTabURL();
 */
 
+window.onload = function() {
+	/*
+	 newDiv = document.createElement("div");
+  newContent = document.createTextNode("Hi there and greetings!");
+  newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+  // add the newly created element and it's content into the DOM
+  my_div = document.getElementById("lga");
+  document.body.insertBefore(newDiv, my_div);
+
+button.id = "goose";
+button.style.backgroundColor = "BLUE";
+	*/
+	
+	tar = document.getElementById("lga");
+	tar.style.backgroundColor = "blue";
+	
+	newDiv = document.createElement("div");
+  newContent = document.createTextNode("Hi there and greetings!");
+  newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+tar.appendChild(newDiv);
+}
+	
+	 
+	
+
+
+
+
+alert("end of content script");
