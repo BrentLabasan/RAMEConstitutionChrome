8db464dd3d09d22103beba9d92d411fd31912e8a
Brent Labasan <BrentLabasan@gmail.com>
2011-11-04 02:04:16 -0700

    div in absolute position now appears in top right corner, scrolls with page

--- contentscript.js
+++ contentscript.js
@@ -2,48 +2,48 @@
  * @author Brent Labasan
  */
 
-alert("content script") ;// pops up every time a new page is loaded
+alert("content script");
+
+// pops up every time a new page is loaded
 // document.getElementById('header-img-a').innerHTML = 'Fred Flinstone'; // works on reddit.com
 
-
 /* // doesn't work
-function displayTabURL() {
-	chrome.tabs.getSelected(null, function(tab) {
-                  alert(tab.url);
-        });
-}
+ function displayTabURL() {
+ chrome.tabs.getSelected(null, function(tab) {
+ alert(tab.url);
+ });
+ }
 
-displayTabURL();
-*/
+ displayTabURL();
+ */
 
 window.onload = function() {
-	/*
-	 newDiv = document.createElement("div");
-  newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent); //add the text node to the newly created div.
-
-  // add the newly created element and it's content into the DOM
-  my_div = document.getElementById("lga");
-  document.body.insertBefore(newDiv, my_div);
-
-button.id = "goose";
-button.style.backgroundColor = "BLUE";
-	*/
-	
-	tar = document.getElementById("lga");
-	tar.style.backgroundColor = "blue";
+	//
+	//spar = document.getElementsByClassName("body");
+	//raps = spar[0];
 	
 	newDiv = document.createElement("div");
-  newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent); //add the text node to the newly created div.
+	newContent = document.createTextNode("Hi there and greetings!");
+	newDiv.appendChild(newContent);
+	
+	newDiv.style.backgroundColor = "blue";
+	newDiv.style.position = "absolute" ;
+	//newDiv.style.marginRight = "10px";
+	//newDiv.style.marginTop = "15px";
+	newDiv.style.width = "50px";
+	newDiv.style.height = "500px";
+	newDiv.style.top = "20px";
+	newDiv.style.right = "20px";
+	newDiv.style.zIndex = "9999999999";
+	newDiv.id = "fuckface";
+	//add the text node to the newly created div.
 
-tar.appendChild(newDiv);
+	//raps.appendChild(newDiv);
+	//document.body.appendChild(newDiv);
+	//cool = document.getElementsByTagName('body')[0];
+	//cool = document.getElementById('masthead');
+	//cool.appendChild(newDiv) ;
+	document.body.appendChild(newDiv);
 }
-	
-	 
-	
-
-
-
 
 alert("end of content script");
