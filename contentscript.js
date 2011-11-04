0e417ec0dfbaed13326d8d6238496cbf642975a5
Brent Labasan <BrentLabasan@gmail.com>
2011-11-04 16:11:42 -0700

    voting window can be clicked to toggle visibility

--- contentscript.js
+++ contentscript.js
@@ -26,30 +26,56 @@ window.onload = function() {
 	newContent = document.createTextNode("Hi there and greetings!");
 	newDiv.appendChild(newContent);
 	
-	newDiv.style.backgroundColor = "blue";
+	newDiv.style.backgroundColor = "GREY";
 	newDiv.style.position = "absolute" ;
 	//newDiv.style.marginRight = "10px";
 	//newDiv.style.marginTop = "15px";
-	newDiv.style.width = "50px";
-	newDiv.style.height = "500px";
+	newDiv.style.width = "320px";
+	newDiv.style.height = "86px";
 	newDiv.style.top = "20px";
-	newDiv.style.right = "300px";
+	newDiv.style.right = "40px";
 	newDiv.style.zIndex = "9999999999";
+	newDiv.style.borderWidth = "thick";
+	newDiv.style.borderColor = "black";
 	newDiv.id = "fuckface";
 	//add the text node to the newly created div.
+
+	votingWindow = document.createElement("div");
+	votingWindowText = document.createTextNode("Voting window text.");
+	votingWindow.appendChild(votingWindowText);
+	
+	votingWindow.style.backgroundColor = "BLUE";
+	votingWindow.style.position = "absolute" ;
+	votingWindow.style.width = "320px";
+	votingWindow.style.height = "700px";
+	votingWindow.style.top = "20px";
+	votingWindow.style.right = "380px";
+	votingWindow.style.zIndex = "9999999999";
+	votingWindow.style.borderWidth = "thick";
+	votingWindow.style.borderColor = "black";
+	votingWindow.id = "votingWindow";
 
 	//raps.appendChild(newDiv);
 	//document.body.appendChild(newDiv);
 	//cool = document.getElementsByTagName('body')[0];
 	//cool = document.getElementById('masthead');
 	//cool.appendChild(newDiv) ;
+	
 	document.body.appendChild(newDiv);
-	$("#fuckface").click(function(){alert("gee")});
+	document.body.appendChild(votingWindow);
+	
+	//$("#fuckface").click(function(){alert("gee")});
 	
 	//newDiv.innerHTML(" <img src='chrome-extension://@@extension_id/images/RAMEdotConstitution_logo300x46.png' />" )
 	var myid = chrome.i18n.getMessage("@@extension_id");
 	smang = " <img src='chrome-extension://" + myid + "/images/RAMEdotConstitution_logo300x46.png' />" ;
+	smang += "<br/><div id='clickHereToVote'>CLICK HERE TO VOTE</div>" ;
 	$("#fuckface").html(smang);
+	
+	$("#clickHereToVote").click(function() {
+		$("#votingWindow").toggle('slow');
+	});
+	
 //alert("end of onload script");
 }
 
